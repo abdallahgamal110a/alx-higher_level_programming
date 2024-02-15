d95b866df0a1509f7fca64f2b25d183974526896
abdallahgamal <abdallahgamal840@gmail.com>
2024-02-15 21:24:21 +0200

    update

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Insert values
+-- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- Insert values
+-- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO
+    first_table
+VALUES
+    (89, "Best School")
