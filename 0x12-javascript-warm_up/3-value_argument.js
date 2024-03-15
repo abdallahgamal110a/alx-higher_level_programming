ab87c244dce8025b6368618e42e3b6319c8bd99b
abdallahgamal110 <abdallahgamal840@gmail.com>
2024-03-15 15:50:59 +0200

    updata

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * script that prints the first argument passed to it
+ */
+
+const arg = process.argv[2]
+if (arg == undefined)
+{
+    console.log('No argument');
+}
+else
+{
+    console.log(arg);    
+}
