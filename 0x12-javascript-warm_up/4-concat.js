cef0021e8d9fcd84aabba550d69d2e288392004f
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-03-20 01:49:15 +0200

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,4 @@
  * script that prints two arguments passed to it, in the following format: “ is ”
  */
 const myarg = process.argv;
-console.log(myarg[2] + " " +'is' + " " + myarg[3]);
+console.log(myarg[2] + ' ' + 'is' + ' ' + myarg[3]);
