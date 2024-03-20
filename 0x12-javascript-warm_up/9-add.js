537d6ecb626bbf0690e448e037dc805a348f6bca
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-03-20 02:56:37 +0200

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,8 @@
 // prints the addition of 2 integers
 
 function add (a, b) {
-  return parseInt(a) + parseInt(b);
+  const sum = parseInt(a) + parseInt(b);
+  return sum;
 }
 
 console.log(add(process.argv[2], process.argv[3]));
